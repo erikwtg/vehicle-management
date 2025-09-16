c0fd0bd0398473d9163d858f08869b3ffd552cc4
Erik William Tahara Gonçalves <erikwtg@gmail.com>
2025-09-16 15:23:25 -0300

    Ajuste nas configurações de comunicação entre serviços dos containers.

--- deployment.sh
+++ deployment.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-if docker network inspect vehicle_management_chat_network &>/dev/null; then
-  echo "Network vehicle_management_chat_network already exists."
+if docker network inspect vehicle_management_network &>/dev/null; then
+  echo "Network vehicle_management_network already exists."
 else
-  docker network create vehicle_management_chat_network
-  echo "Network vehicle_management_chat_network created."
+  docker network create vehicle_management_network
+  echo "Network vehicle_management_network created."
 fi
 
 docker-compose -f docker-compose.yml up -d --build
 
 echo "Containers started successfully."
 
-if docker ps --filter "network=vehicle_management_chat_network" | grep -q "api"; then
+if docker ps --filter "network=vehicle_management_network" | grep -q "api"; then
   echo "Container is running and connected to the network."
 else
   echo "Container is not running or not connected to the network."
